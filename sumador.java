3808fc8d9413ab59121967a0fa244509f41de9a0
pablotejerina <pabloteje09@gmail.com>
2020-10-09 11:55:51 +0200

    aaa

---
+++ sumador.java
@@ -0,0 +1,13 @@
+/**
+ * sumador
+ */
+public class sumador {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 2;
+        int suma = num1 + num2;
+        System.out.println("La suma vale:" + suma);
+
+    }
+}
